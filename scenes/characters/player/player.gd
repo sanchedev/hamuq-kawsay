f09be4fb8698637c1df6900dfe52f39cc4be44a7
José Sánchez <sanchedev@outlook.com>
2025-06-29 22:06:26 -0500

    Agregando las animaciones direccionales al jugador

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,16 +5,22 @@ class_name Player extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var input := Vector2.ZERO
+var sufix: String = "down"
 
 func _physics_process(delta: float) -> void:
 	input.x = Input.get_axis("left", "right")
 	input.y = Input.get_axis("up", "down")
 	
 	# animations
+	if input.y < 0: sufix = "up"
+	elif input.y > 0: sufix = "down"
+	elif input.x > 0: sufix = "right"
+	elif input.x < 0: sufix = "left"
+	
 	if input.x != 0 or input.y != 0:
-		animated_sprite_2d.play("walk")
+		animated_sprite_2d.play("walk_"+sufix)
 	else:
-		animated_sprite_2d.play("idle")
+		animated_sprite_2d.play("idle_"+sufix)
 	# movement
 	input.normalized()
 	velocity = input * speed * delta
