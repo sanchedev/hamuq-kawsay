35202a4f7f44560aebccad93fa82c937e8f0607d
José Sánchez <sanchedev@outlook.com>
2025-07-07 17:54:48 -0500

    Creando tileset y escena de ejemplo

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,6 +1,7 @@
 class_name Player extends CharacterBody2D
 
-@export var speed = 3200
+@export var speed = 40
+@export var run_speed = 80
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -11,6 +12,8 @@ func _physics_process(delta: float) -> void:
 	input.x = Input.get_axis("left", "right")
 	input.y = Input.get_axis("up", "down")
 	
+	var is_running := Input.is_action_pressed("run")
+	
 	# animations
 	if input.y < 0: sufix = "up"
 	elif input.y > 0: sufix = "down"
@@ -18,10 +21,16 @@ func _physics_process(delta: float) -> void:
 	elif input.x < 0: sufix = "left"
 	
 	if input.x != 0 or input.y != 0:
-		animated_sprite_2d.play("walk_"+sufix)
+		if is_running:
+			animated_sprite_2d.play("run_"+sufix)
+		else:
+			animated_sprite_2d.play("walk_"+sufix)
 	else:
 		animated_sprite_2d.play("idle_"+sufix)
 	# movement
-	input.normalized()
-	velocity = input * speed * delta
+	velocity = input.normalized()
+	if is_running:
+		velocity *= run_speed
+	else:
+		velocity *= speed
 	move_and_slide()
